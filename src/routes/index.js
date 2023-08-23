ed60270d6820fffebd867d619900e6bd63d316d2
Luiz Silva <luiz.silva@grupozelo.com>
2023-08-23 14:01:27 -0300

    Inserindo icones tabbar

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,60 +6,95 @@ import Login from "../pages/Login";
 import Home from "../pages/Home";
 import Cadastro from "../pages/Cadastro";
 
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faCircleUser, faHandshake } from '@fortawesome/free-regular-svg-icons';
-
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
 
 
 function Tabs() {
-    return (
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Inicio"
-          component={Home}
-          options={{
-            headerShown: false,
-            tabBarIcon: () => (
-              <FontAwesomeIcon name="home" color={'black'} size={25} icon={ faCircleUser }/>
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Cadastro"
-          component={Cadastro}
-          options={{
-            headerShown: false,
-            tabBarIcon: () => (
-                <FontAwesomeIcon name="home" color={'black'} size={25} icon={ faHandshake }/>
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    );
-  }
+  return (
+    <Tab.Navigator
+    screenOptions={{
+      tabBarActiveTintColor: '#000',
+      tabBarInactiveTintColor: '#000',
+      tabBarLabelStyle: { fontSize: 14, fontFamily: 'Montserrat_400Regular'},
+      tabBarItemStyle: { width: 100 },
+      tabBarStyle: { position: 'absolute', backgroundColor: '#FFBE00' },
+    }}
+    >
+      <Tab.Screen
+        name="Conta"
+        component={Home}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="account-circle" color={'black'} size={30} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Financeiro"
+        component={Cadastro}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="cash" color={'black'} size={30} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Inicio"
+        component={Cadastro}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="map-marker-radius-outline" color={'black'} size={30} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Veiculos"
+        component={Cadastro}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="car-back" color={'black'} size={30} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Ajustes"
+        component={Cadastro}
+        options={{
+          headerShown: false,
+          tabBarIcon: () => (
+            <MaterialCommunityIcons name="cog-outline" color={'black'} size={30} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 export default function Routes() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen
-                name="Welcome"
-                component={Welcome}
-                options={{headerShown: false}}
-            />
-            <Stack.Screen
-                name="Login"
-                component={Login}
-                options={{headerShown: false}}
-            />
-            <Stack.Screen
-                name="Home"
-                component={Tabs}
-                options={{headerShown: false}}
-            />
-        </Stack.Navigator>
-    )
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Welcome"
+        component={Welcome}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Home"
+        component={Tabs}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  )
 }
