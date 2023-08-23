8d1848607f2fe86c368c4df47b561a36e78ea048
Luiz Silva <luiz.silva@grupozelo.com>
2023-08-22 23:48:06 -0300

    Criando TabBar

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,46 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Welcome from "../pages/Welcome";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
+import Cadastro from "../pages/Cadastro";
+
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { faCircleUser, faHandshake } from '@fortawesome/free-regular-svg-icons';
+
+
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+
+function Tabs() {
+    return (
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Inicio"
+          component={Home}
+          options={{
+            headerShown: false,
+            tabBarIcon: () => (
+              <FontAwesomeIcon name="home" color={'black'} size={25} icon={ faCircleUser }/>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Cadastro"
+          component={Cadastro}
+          options={{
+            headerShown: false,
+            tabBarIcon: () => (
+                <FontAwesomeIcon name="home" color={'black'} size={25} icon={ faHandshake }/>
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
 
 export default function Routes() {
     return (
@@ -21,7 +57,7 @@ export default function Routes() {
             />
             <Stack.Screen
                 name="Home"
-                component={Home}
+                component={Tabs}
                 options={{headerShown: false}}
             />
         </Stack.Navigator>
