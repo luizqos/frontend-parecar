b1336aa261c021627335f534022414180b107d42
Luiz Silva <luiz.silva@grupozelo.com>
2023-08-26 17:24:45 -0300

    Criando tabBar

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,6 +6,8 @@ import Login from "../pages/Login";
 import Home from "../pages/Home";
 import Cadastro from "../pages/Cadastro";
 
+import BottonNew from "../components/ButtonNew"
+
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Stack = createNativeStackNavigator();
@@ -14,22 +16,30 @@ const Tab = createBottomTabNavigator();
 
 function Tabs() {
   return (
-    <Tab.Navigator
+    <Tab.Navigator initialRouteName="Inicio"
     screenOptions={{
-      tabBarActiveTintColor: '#000',
+      tabBarActiveTintColor: '#0000FF',
       tabBarInactiveTintColor: '#000',
-      tabBarLabelStyle: { fontSize: 12, fontFamily: 'Montserrat_700Bold'},
-      tabBarItemStyle: { width: 100 },
-      tabBarStyle: { position: 'absolute', backgroundColor: '#FFBE00' },
+      tabBarLabelStyle: { fontSize: 12,
+         fontFamily: 'Montserrat_700Bold'
+        },
+      tabBarItemStyle: { 
+        width: 100 
+      },
+      tabBarStyle: { 
+        position: 'absolute', 
+        backgroundColor: '#FFBE00', 
+        borderTopColor: 'transparent'
+      },
     }}
     >
       <Tab.Screen
         name="Conta"
-        component={Home}
+        component={Cadastro}
         options={{
           headerShown: false,
-          tabBarIcon: () => (
-            <MaterialCommunityIcons name="account-circle" color={'black'} size={30} />
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account-circle" color={color} size={size} />
           ),
         }}
       />
@@ -38,18 +48,21 @@ function Tabs() {
         component={Cadastro}
         options={{
           headerShown: false,
-          tabBarIcon: () => (
-            <MaterialCommunityIcons name="cash" color={'black'} size={30} />
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="cash" color={color} size={size} />
           ),
         }}
       />
       <Tab.Screen
         name="Inicio"
-        component={Cadastro}
+        component={Home}
         options={{
           headerShown: false,
-          tabBarIcon: () => (
-            <MaterialCommunityIcons name="map-marker-radius-outline" color={'black'} size={30} />
+          tabBarLabelStyle: {
+            color: 'transparent'
+          },
+          tabBarIcon: ({ color, size }) => (
+            <BottonNew color={color} size={size} />
           ),
         }}
       />
@@ -58,8 +71,9 @@ function Tabs() {
         component={Cadastro}
         options={{
           headerShown: false,
-          tabBarIcon: () => (
-            <MaterialCommunityIcons name="car-back" color={'black'} size={30} />
+
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="car-back" color={color} size={size} />
           ),
         }}
       />
@@ -68,8 +82,8 @@ function Tabs() {
         component={Cadastro}
         options={{
           headerShown: false,
-          tabBarIcon: () => (
-            <MaterialCommunityIcons name="cog-outline" color={'black'} size={30} />
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="cog-outline" color={color} size={size} />
           ),
         }}
       />
