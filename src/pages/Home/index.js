c9f5acec2d614de4e9532aba3976c4b157f78968
Luiz Silva <luiz.silva@grupozelo.com>
2023-08-29 07:13:17 -0300

    Criando Mapa

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,67 +1,129 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
+import { View, StyleSheet, PermissionsAndroid } from "react-native";
 import {
-    View,
-    StyleSheet
-} from "react-native";
+  requestBackgroundPermissionsAsync,
+  getCurrentPositionAsync,
+  LocationObject,
+  watchPositionAsync,
+  LocationAccuracy,
+} from "expo-location";
 
-import * as Animatable from 'react-native-animatable';
+import MapView, { Marker } from "react-native-maps";
 import { useNavigation } from "@react-navigation/native";
 
 export default function Home() {
-    const navigation = useNavigation();
-    return (
-        <View style={styles.container}>
-            <View style={styles.containerLogo}>
-                <Animatable.Image
-                    animation="flipInY"
-                    source={require('../../assets/img/logo.png')}
-                    style={{ width: '75%' }}
-                    resizeMode="contain"
-                />
-            </View>
-        </View>
+  //const [location, setLocation] = useState <LocationObject | null>(null);
+  const [location, setLocation] = useState(null);
+  const [initialRegion, setinitialRegion] = useState(null);
+  const navigation = useNavigation();
 
+  async function requestLocationPermissions() {
+    const { granted } = await requestBackgroundPermissionsAsync();
+    if (granted) {
+      const getCurrentPosition = await getCurrentPositionAsync();
+    }
+  }
+
+  useEffect(() => {
+    requestLocationPermissions();
+  }, []);
+  useEffect(() => {
+    watchPositionAsync(
+      {
+        accuracy: LocationAccuracy.Highest,
+        timeInterval: 1000,
+        distanceInterval: 1,
+      },
+      (reponse) => {
+        setLocation(reponse);
+      }
     );
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      {location ? (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            }}
+            title="Minha Localização"
+          />
+        </MapView>
+      ) : (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: -19.9111285,
+            longitude: -44.0696268,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: -19.9111285,
+              longitude: -44.0696268,
+            }}
+            title="Minha Localização"
+          />
+        </MapView>
+      )}
+    </View>
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#FFBE00'
-    },
-    containerLogo: {
-        flex: 2,
-        backgroundColor: '#E5E5E5',
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-    containerForm: {
-        flex: 1,
-        backgroundColor: '#E5E5E5',
-        borderTopLeftRadius: 25,
-        borderTopRightRadius: 25,
-        paddingStart: '5%',
-        paddingEnd: '5%'
-    },
-    title: {
-        fontSize: 20,
-        fontFamily: 'Montserrat_700Bold',
-        marginTop: 28,
-        marginBottom: 12
-    },
-    button: {
-        position: 'absolute',
-        backgroundColor: '#FFBE00',
-        borderRadius: 50,
-        paddingVertical: 8,
-        width: '60%',
-        alignSelf: 'center',
-        bottom: '15%',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    buttonText: {
-        fontSize: 18,
-        fontFamily: 'Montserrat_700Bold'
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#FFBE00",
+  },
+  map: {
+    flex: 1,
+  },
+  containerLogo: {
+    flex: 2,
+    backgroundColor: "#E5E5E5",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  containerForm: {
+    flex: 1,
+    backgroundColor: "#E5E5E5",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    paddingStart: "5%",
+    paddingEnd: "5%",
+  },
+  title: {
+    fontSize: 20,
+    fontFamily: "Montserrat_700Bold",
+    marginTop: 28,
+    marginBottom: 12,
+  },
+  button: {
+    position: "absolute",
+    backgroundColor: "#FFBE00",
+    borderRadius: 50,
+    paddingVertical: 8,
+    width: "60%",
+    alignSelf: "center",
+    bottom: "15%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 18,
+    fontFamily: "Montserrat_700Bold",
+  },
+});
