46f9679558478c0c2563471a8c09f50c0b8591ab
Luiz Silva <luiz.silva@grupozelo.com>
2023-11-11 22:21:58 -0300

    Buscar cidade por lat e long

--- src/services/EstacionamentoService.js
+++ src/services/EstacionamentoService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Config from "../util/Config";
 
 class EstacionamentoService {
-  async buscaEstacionamento() {
+  async buscaEstacionamento(dados) {
     axios.interceptors.request.use(
       (request) => {
         return request;
@@ -13,9 +13,77 @@ class EstacionamentoService {
       }
     );
 
+    let {
+      latitude = null,
+      longitude = null,
+      cidade = null,
+      uf = null,
+      entrada = null,
+      saida = null,
+    } = dados;
+
+    const apiKey = "AIzaSyCiSR-itPpBtfKDoZC96k0vQOGKF6qsmEk";
+    const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${apiKey}`;
+
+    const buscaCidadeUF = async (apiUrl) => {
+      try {
+        const response = await axios.get(apiUrl);
+
+        if (response.data.status !== "OK") {
+          throw new Error("Erro na solicitação: " + response.data.status);
+        }
+
+        const results = response.data.results;
+
+        if (results.length === 0) {
+          throw new Error("Nenhum resultado encontrado.");
+        }
+
+        const addressComponents = results[0].address_components;
+        let city, state;
+
+        for (const component of addressComponents) {
+          if (component.types.includes("administrative_area_level_2")) {
+            city = component.long_name;
+          } else if (component.types.includes("administrative_area_level_1")) {
+            state = component.short_name;
+          }
+        }
+
+        if (city && state) {
+          return { city, state };
+        } else {
+          throw new Error("Cidade e UF não encontradas.");
+        }
+      } catch (error) {
+        throw new Error("Erro na solicitação: " + error.message);
+      }
+    };
     try {
+      if (latitude && longitude) {
+        const responseLocation = await buscaCidadeUF(apiUrl);
+        const city = responseLocation.city.toUpperCase();
+        const state = responseLocation.state.toUpperCase();
+
+        uf = state;
+        cidade = city;
+
+        const dataAtual = new Date(new Date().getTime() + 30 * 60 * 1000);
+        const minutosArredondados = Math.ceil(dataAtual.getMinutes() / 15) * 15;
+        dataAtual.setMinutes(minutosArredondados);
+        dataAtual.setSeconds(0);
+        entrada = new Date(dataAtual.getTime() - 3 * 60 * 60 * 1000)
+          .toISOString()
+          .slice(0, 19)
+          .replace("T", " ");
+        saida = new Date(dataAtual.getTime() - 1 * 60 * 60 * 1000)
+          .toISOString()
+          .slice(0, 19)
+          .replace("T", " ");
+      }
+
       const response = await axios.get(
-        `${Config.API_URL}/estacionamentos?status=1`,
+        `${Config.API_URL}/estacionamentos/vagas-disponiveis?uf=${uf}&cidade=${cidade}&entradareserva=${entrada}&saidareserva=${saida}`,
         {
           timeout: Config.TIMEOUT_REQUEST,
           headers: Config.HEADER_REQUEST,
